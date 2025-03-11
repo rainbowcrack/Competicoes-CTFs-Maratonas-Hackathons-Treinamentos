385f537497713ea52d83e6b67d8151d7074b3355
Izabel Chaves <145824500+rainbowcrack@users.noreply.github.com>
2025-03-11 14:03:52 -0300

    Create again.cpp

---
+++ Curso-PUC-Programacao-Competitiva/vjudge/again.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    // todo numero elevado 5^n, termina em 25
+    int n;
+    cin >> n;
+
+    cout << 25 << endl;
+    return 0;
+}
