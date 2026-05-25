aa7fc75e63cce7b20d69ce0f2662775128a13af8
Izabel Chaves <145824500+rainbowcrack@users.noreply.github.com>
2026-05-25 07:49:08 -0300

    Create a.cpp

---
+++ Maratona-feminina-de-programacao/2026/a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    
+    int X; cin >> X;
+    int Y; cin >> Y;
+    
+    cout << X*Y << endl;
+ 
+    return 0;
+}
